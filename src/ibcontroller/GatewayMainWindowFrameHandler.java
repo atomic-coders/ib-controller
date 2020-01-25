7de77beffaca03b11c5de93c722e8f198edd8d2c
Carsten Wolters <c.wolters@gmx.de>
2020-01-25 11:28:50 +0100

    added debug info

--- src/ibcontroller/GatewayMainWindowFrameHandler.java
+++ src/ibcontroller/GatewayMainWindowFrameHandler.java
@@ -26,7 +26,7 @@ class GatewayMainWindowFrameHandler  implements WindowHandler {
     @Override
     public boolean filterEvent(Window window, int eventId) {
         switch (eventId) {
-            case WindowEvent.WINDOW_ACTIVATED:
+            case WindowEvent.WINDOW_OPENED:
                 return true;
             default:
                 return false;
